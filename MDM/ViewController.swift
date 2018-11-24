96926c8e7ee263b01374c5d175dc8c99337c274e
mi-nakano <mizuki590@gmail.com>
2018-11-24 10:06:56 +0900

    generate files from Xcode

---
+++ MDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MDM
+//
+//  Created by mizunaka on 2018/11/24.
+//  Copyright © 2018 chukichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
