484aeb4ffa0310ca50dc379a70d992694e1998fa
mi-nakano <mizuki590@gmail.com>
2018-11-24 13:51:34 +0900

    set uiimageview

--- MDM/ViewController.swift
+++ MDM/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var bezierPath: UIBezierPath!
+    @IBOutlet var canvas: UIImageView!
+    var lastDrawImage: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +20,46 @@ class ViewController: UIViewController {
     }
 
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touchEvent = touches.first!
+        let currentPoint: CGPoint = touchEvent.location(in: self.canvas)
+        bezierPath = UIBezierPath()
+        bezierPath.lineWidth = 4.0
+        bezierPath.lineCapStyle = .butt
+        bezierPath.move(to:currentPoint)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if bezierPath == nil {
+            return
+        }
+        let touchEvent = touches.first!
+        let currentPoint:CGPoint = touchEvent.location(in: self.canvas)
+        bezierPath.addLine(to: currentPoint)
+        drawLine(path: bezierPath)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if bezierPath == nil {
+            return
+        }
+        let touchEvent = touches.first!
+        let currentPoint: CGPoint = touchEvent.location(in: canvas)
+        bezierPath.addLine(to: currentPoint)
+        drawLine(path: bezierPath)
+        self.lastDrawImage = canvas.image
+    }
+
+    func drawLine(path:UIBezierPath){
+        UIGraphicsBeginImageContext(canvas.frame.size)
+        if let image = self.lastDrawImage {
+            image.draw(at: CGPoint.zero)
+        }
+        let lineColor = UIColor.black
+        lineColor.setStroke()
+        path.stroke()
+        self.canvas.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
 }
 
